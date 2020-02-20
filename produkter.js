48547e183047d5b0dade004ce3337330669ef4e6
Laura <lauraragnars@Lauras-MacBook-Pro-3.local>
2020-02-20 20:07:15 +0100

    produkter grid virker og json sat ind

---
+++ produkter.js
@@ -0,0 +1,36 @@
+let produkterListe = [];
+
+const produktTemplate = document.querySelector("template");
+const container = document.querySelector(".data-container");
+let filter = "alle";
+
+const link = "https://spreadsheets.google.com/feeds/list/1KFeuCYN5F8ByEdt8BDCMkvBp2RHG39MfIKpr_OZD3AU/od6/public/values?alt=json";
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    hentData();
+    //addEventlistenersToAllButtons();
+}
+
+async function hentData() {
+    const response = await fetch(link);
+    produkterListe = await response.json();
+    visProdukter();
+}
+
+function visProdukter() {
+    container.innerHTML = "";
+    produkterListe.feed.entry.forEach(produkt => {
+
+        if (filter == "alle" || filter == produkt.gsx$type.$t) {
+            let klon = produktTemplate.cloneNode(true).content;
+            klon.querySelector("h2").textContent = produkt.gsx$navn.$t;
+            klon.querySelector("h3").textContent = produkt.gsx$year.$t;
+            klon.querySelector(".type").textContent = produkt.gsx$type.$t;
+            klon.querySelector(".origin").textContent = "Oprindelse: " + produkt.gsx$origin.$t;
+
+            container.appendChild(klon);
+        }
+    })
+}
