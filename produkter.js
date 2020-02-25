b784ef7654b7f1eabe7dffac79498fc25e125a4a
Laura <lauraragnars@Lauras-MacBook-Pro-3.local>
2020-02-25 10:05:23 +0100

    menu ændringer

--- produkter.js
+++ produkter.js
@@ -1,24 +1,25 @@
+// Det tomme array til produkterne
 let produkterListe = [];
 
+// alle mulige variabler
 const produktTemplate = document.querySelector("template");
 const container = document.querySelector(".data-container");
 let filter = "alle";
-
 const detalje = document.querySelector("#detalje");
 const lukKnap = document.querySelector("#luk");
 
-
-
-
+//link til json spreadsheet
 const link = "https://spreadsheets.google.com/feeds/list/1KFeuCYN5F8ByEdt8BDCMkvBp2RHG39MfIKpr_OZD3AU/od6/public/values?alt=json";
 
 document.addEventListener("DOMContentLoaded", start);
 
+// function som kører efter dom er loaded
 function start() {
     hentData();
-    //addEventlistenersToAllButtons();
+    addEventlistenersToAllButtons();
 }
 
+// henter json data
 async function hentData() {
     const response = await fetch(link);
     produkterListe = await response.json();
@@ -26,12 +27,14 @@ async function hentData() {
 
 }
 
+// sætter info fra json data ind i template
 function visProdukter() {
-    container.innerHTML = "";
+    container.innerHTML = ""; // tømmer container
     produkterListe.feed.entry.forEach(produkt => {
 
+        // if sætning med filter funktion
         if (filter == "alle" || filter == produkt.gsx$type.$t) {
-            let klon = produktTemplate.cloneNode(true).content;
+            let klon = produktTemplate.cloneNode(true).content; // kloner templaten og sætter inhold ind
             klon.querySelector("h2").textContent = produkt.gsx$navn.$t;
             klon.querySelector("h3").textContent = produkt.gsx$year.$t;
             klon.querySelector(".type").textContent = produkt.gsx$type.$t;
@@ -42,6 +45,7 @@ function visProdukter() {
 
             container.appendChild(klon);
 
+            // sætter event listener på det sidste man trykker paa og linker til detalje view
             container.lastElementChild.addEventListener("click", () => {
                 location.href = "produktside.html?id=" + produkt.gsx$id.$t;
 
@@ -50,6 +54,8 @@ function visProdukter() {
     })
 }
 
+
+// sætter event listener paa alle filter knapper
 function addEventlistenersToAllButtons() {
     document.querySelectorAll(".filter").forEach(elm => {
         elm.addEventListener("click", filtrering);
@@ -57,16 +63,18 @@ function addEventlistenersToAllButtons() {
     })
 }
 
+// filtrerer produkter
 function filtrering() {
     console.log("FILTER");
     filter = this.dataset.produkt;
-    //    document.querySelector("header h1").textContent = this.textContent;
     document.querySelectorAll(".filter").forEach(elm2 => {
         elm2.classList.remove("valgt");
     })
     this.classList.add("valgt");
     visProdukter();
 }
+
+
 
 //burgermenu
 
